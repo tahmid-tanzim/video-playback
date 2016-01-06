6b3209e7d793ced269f546c7449ed9976836b8f3
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2016-01-06 20:13:58 +0600

    Playback progress meters

--- app/controller.js
+++ app/controller.js
@@ -1,4 +1,9 @@
 videoApp.controller('VideoController', ['$scope', '$interval', function($scope, $interval) {
+    $scope.scrubTop = -1000;
+    $scope.scrubLeft = -1000;
+    $scope.vidHeightCenter = -1000;
+    $scope.vidWidthCenter = -1000;
+
     $scope.video = {
         title: 'Starlight Scamper',
         description: 'Starlight Scamper: A video player build with AngularJS',
@@ -14,7 +19,12 @@ videoApp.controller('VideoController', ['$scope', '$interval', function($scope, 
         $scope.video.element.addEventListener('loadedmetadata', $scope.updateData, true);
     };
 
-    $interval(function(){
+    $interval(function() {
+        var currentTime = $scope.video.element.currentTime;
+        var totalTime = $scope.video.element.duration;
+        //var viewedPercent = currentTime / totalTime * 100;
+        var progressLeftWidth = document.getElementById('progressMeterFull').offsetLeft + document.getElementById('progressMeterFull').offsetWidth;
+        $scope.scrubLeft = (currentTime / totalTime * progressLeftWidth) - 7;
         $scope.updateLayout();
     }, 100);
 
@@ -24,45 +34,49 @@ videoApp.controller('VideoController', ['$scope', '$interval', function($scope, 
             $scope.video.element.pause();
             $scope.video.isPlaying = false;
             $scope.video.currentTime = 0;
-            $('#playBtn').children("span").toggleClass("glyphicon-play", true);
-            $('#playBtn').children("span").toggleClass("glyphicon-pause", false);
+            $('#playBtn').children('span').toggleClass('glyphicon-play', true);
+            $('#playBtn').children('span').toggleClass('glyphicon-pause', false);
         }
-        $scope.updateLayout();
     };
 
     $scope.updateData = function(e) {
         $scope.video.totalTime = e.target.duration;
     };
 
+    /* Update video time */
     $scope.updateLayout = function() {
+        $scope.scrubTop = document.getElementById('progressMeterFull').offsetTop-2;
+        $scope.vidHeightCenter = $scope.video.element.offsetHeight/2 - 50;
+        $scope.vidWidthCenter = $scope.video.element.offsetWidth/2 - 50;
+
         if(!$scope.$$phase) {
             $scope.$apply();
         }
     };
 
     $scope.togglePlay = function() {
-        if($scope.video.element.paused){
+        if($scope.video.element.paused) {
             $scope.video.element.play();
             $scope.video.isPlaying = true;
-            $('#playBtn').children("span").toggleClass("glyphicon-play", false);
-            $('#playBtn').children("span").toggleClass("glyphicon-pause", true);
-        }else{
+            $('#playBtn').children('span').toggleClass('glyphicon-play', false);
+            $('#playBtn').children('span').toggleClass('glyphicon-pause', true);
+        } else {
             $scope.video.element.pause();
             $scope.video.isPlaying = false;
-            $('#playBtn').children("span").toggleClass("glyphicon-play", true);
-            $('#playBtn').children("span").toggleClass("glyphicon-pause", false);
+            $('#playBtn').children('span').toggleClass('glyphicon-play', true);
+            $('#playBtn').children('span').toggleClass('glyphicon-pause', false);
         }
     };
 
     $scope.toggleMute = function() {
         if($scope.video.element.volume == 0.0){
             $scope.video.element.volume = 1.0;
-            $('#muteBtn').children("span").toggleClass("glyphicon-volume-up", true);
-            $('#muteBtn').children("span").toggleClass("glyphicon-volume-off", false);
-        }else{
+            $('#muteBtn').children('span').toggleClass('glyphicon-volume-up', true);
+            $('#muteBtn').children('span').toggleClass('glyphicon-volume-off', false);
+        } else {
             $scope.video.element.volume = 0.0;
-            $('#muteBtn').children("span").toggleClass("glyphicon-volume-up", false);
-            $('#muteBtn').children("span").toggleClass("glyphicon-volume-off", true);
+            $('#muteBtn').children('span').toggleClass('glyphicon-volume-up', false);
+            $('#muteBtn').children('span').toggleClass('glyphicon-volume-off', true);
         }
     };
 
