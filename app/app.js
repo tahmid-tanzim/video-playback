0422fa88995fae55e1a914c86d40f115f2b4a24f
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2016-01-06 17:08:17 +0600

    Playback and mute controls

--- app/app.js
+++ app/app.js
@@ -1,7 +1,36 @@
 var videoApp = angular.module('videoApp', []);
 
 videoApp.controller('VideoController', ['$scope', function($scope) {
+    $scope.videoDisplay = document.getElementById("videoElement");
     $scope.videoSource = 'video/StarlightScamper.mp4';
     $scope.titleDisplay = 'Starlight Scamper';
     $scope.videoDescription = 'Starlight Scamper: A video player build with AngularJS';
+    $scope.videoPlaying = false;
+
+    $scope.togglePlay = function() {
+        if($scope.videoDisplay.paused){
+            $scope.videoDisplay.play();
+            $scope.videoPlaying = true;
+            $('#playBtn').children("span").toggleClass("glyphicon-play", false);
+            $('#playBtn').children("span").toggleClass("glyphicon-pause", true);
+        }else{
+            $scope.videoDisplay.pause();
+            $scope.videoPlaying = false;
+            $('#playBtn').children("span").toggleClass("glyphicon-play", true);
+            $('#playBtn').children("span").toggleClass("glyphicon-pause", false);
+        }
+    };
+
+    $scope.toggleMute = function() {
+        if($scope.videoDisplay.volume == 0.0){
+            $scope.videoDisplay.volume = 1.0;
+            $('#muteBtn').children("span").toggleClass("glyphicon-volume-up", true);
+            $('#muteBtn').children("span").toggleClass("glyphicon-volume-off", false);
+        }else{
+            $scope.videoDisplay.volume = 0.0;
+            $('#muteBtn').children("span").toggleClass("glyphicon-volume-up", false);
+            $('#muteBtn').children("span").toggleClass("glyphicon-volume-off", true);
+        }
+    };
+
 }]);
